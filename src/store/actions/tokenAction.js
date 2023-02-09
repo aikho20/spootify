8c95bf501e4f4192083d7bfee9e9663e55cf27b5
aikho20 <104066597+aikho20@users.noreply.github.com>
2023-02-09 08:42:29 +0800

    sixth commit

--- src/store/actions/tokenAction.js
+++ src/store/actions/tokenAction.js
@@ -5,22 +5,9 @@ import { RequestsEnum } from '../enum'
 
 export const getAccessToken = (payload) => async (dispatch) => {
     await requestHelper(dispatch, RequestsEnum.getAccessToken, async () => {
-        const authParams = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-                'Access-Control-Allow-Origin': 'http://localhost:3000',
-            },
-            data:
-                'grant_type=client_credentials&client_id=' +
-                process.env.REACT_APP_SPOTIFY_CLIENT_ID +
-                '&client_secret=' +
-                process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
-        }
-        const token = await axios.post('https://accounts.spotify.com/api/token', authParams)
         dispatch({
             type: GET_ACCESS_TOKEN,
-            payload: { token: token },
+            payload: { token: 'testing' },
         })
     })
 }
